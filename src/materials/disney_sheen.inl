4cfd1c59f38e9bac7589bf6d06cdc8e685db09ed
wuyah <lussiouserror@gmail.com>
2025-01-12 10:30:51 -0800

    Finish Glass & Sheen

--- src/materials/disney_sheen.inl
+++ src/materials/disney_sheen.inl
@@ -11,9 +11,20 @@ Spectrum eval_op::operator()(const DisneySheen &bsdf) const {
     if (dot(frame.n, dir_in) < 0) {
         frame = -frame;
     }
+    Vector3 half_vector = normalize( dir_in + dir_out );
+
+    Real h_dot_out = dot( half_vector, dir_out) ;
+    Real n_dot_out = dot( frame.n, dir_out );
+
+    Spectrum base_color = eval(bsdf.base_color, vertex.uv, vertex.uv_screen_size, texture_pool);
+    Real L = luminance(base_color);
+    Spectrum c_tint = L > 0 ? base_color / L : make_const_spectrum(1);
+    Real sheen_tint = eval(bsdf.sheen_tint, vertex.uv, vertex.uv_screen_size, texture_pool);
+    Spectrum c_sheen = (1 - sheen_tint) + sheen_tint * c_tint;
+    Spectrum f_tint = c_sheen * pow( 1 - fabs(h_dot_out), Real(5) ) * fabs(n_dot_out);
 
     // Homework 1: implement this!
-    return make_zero_spectrum();
+    return f_tint;
 }
 
 Real pdf_sample_bsdf_op::operator()(const DisneySheen &bsdf) const {
@@ -29,7 +40,7 @@ Real pdf_sample_bsdf_op::operator()(const DisneySheen &bsdf) const {
     }
 
     // Homework 1: implement this!
-    return 0;
+    return fmax(dot(frame.n, dir_out), Real(0)) / c_PI;
 }
 
 std::optional<BSDFSampleRecord>
@@ -45,7 +56,9 @@ std::optional<BSDFSampleRecord>
     }
 
     // Homework 1: implement this!
-    return {};
+    return BSDFSampleRecord{
+        to_world(frame, sample_cos_hemisphere(rnd_param_uv)),
+        Real(0) /* eta */, Real(1)};  /* roughness */
 }
 
 TextureSpectrum get_texture_op::operator()(const DisneySheen &bsdf) const {
--- src/microfacet.h
+++ src/microfacet.h
@@ -129,38 +129,6 @@ inline Vector3 sample_visible_normals(const Vector3 &local_dir_in, Real alpha, c
 }
 
 
-/// add anistropic based on sample_visible_normals
-inline Vector3 sample_visible_normals_metal(const Vector3 &local_dir_in, Vector2 alpha, const Vector2 &rnd_param) {
-    // The incoming direction is in the "ellipsodial configuration" in Heitz's paper
-    if (local_dir_in.z < 0) {
-        // Ensure the input is on top of the surface.
-        return -sample_visible_normals_metal(-local_dir_in, alpha, rnd_param);
-    }
-
-    // Transform the incoming direction to the "hemisphere configuration".
-    Vector3 hemi_dir_in = normalize(
-        Vector3{alpha.x * local_dir_in.x, alpha.y * local_dir_in.y, local_dir_in.z});
-
-    // Parameterization of the projected area of a hemisphere.
-    // First, sample a disk.
-    Real r = sqrt(rnd_param.x);
-    Real phi = 2 * c_PI * rnd_param.y;
-    Real t1 = r * cos(phi);
-    Real t2 = r * sin(phi);
-    // Vertically scale the position of a sample to account for the projection.
-    Real s = (1 + hemi_dir_in.z) / 2;
-    t2 = (1 - s) * sqrt(1 - t1 * t1) + s * t2;
-    // Point in the disk space
-    Vector3 disk_N{t1, t2, sqrt(max(Real(0), 1 - t1*t1 - t2*t2))};
-
-    // Reprojection onto hemisphere -- we get our sampled normal in hemisphere space.
-    Frame hemi_frame(hemi_dir_in);
-    Vector3 hemi_N = to_world(hemi_frame, disk_N);
-
-    // Transforming the normal back to the ellipsoid configuration
-    return normalize(Vector3{alpha.x * hemi_N.x, alpha.y * hemi_N.y, max(Real(0), hemi_N.z)});
-}
-
 // Sample stratgy for disney clear code.
 inline Vector3 sample_visible_normals_cc(const Vector3 &local_dir_in,const Real alpha, const Vector2 &rnd_param) {
     // The incoming direction is in the "ellipsodial configuration" in Heitz's paper
@@ -190,4 +158,36 @@ inline Vector3 sample_visible_normals_cc(const Vector3 &local_dir_in,const Real 
 
     // Transforming the normal back to the ellipsoid configuration
     return normalize(Vector3{alpha * hemi_N.x, alpha * hemi_N.y, max(Real(0), hemi_N.z)});
+}
+
+// Sample method for disney glass:
+inline Vector3 sample_visible_normals_ani(const Vector3 &local_dir_in, Vector2 alpha, const Vector2 &rnd_param) {
+    // The incoming direction is in the "ellipsodial configuration" in Heitz's paper
+    if (local_dir_in.z < 0) {
+        // Ensure the input is on top of the surface.
+        return -sample_visible_normals_ani(-local_dir_in, alpha, rnd_param);
+    }
+
+    // Transform the incoming direction to the "hemisphere configuration".
+    Vector3 hemi_dir_in = normalize(
+        Vector3{alpha.x * local_dir_in.x, alpha.y * local_dir_in.y, local_dir_in.z});
+
+    // Parameterization of the projected area of a hemisphere.
+    // First, sample a disk.
+    Real r = sqrt(rnd_param.x);
+    Real phi = 2 * c_PI * rnd_param.y;
+    Real t1 = r * cos(phi);
+    Real t2 = r * sin(phi);
+    // Vertically scale the position of a sample to account for the projection.
+    Real s = (1 + hemi_dir_in.z) / 2;
+    t2 = (1 - s) * sqrt(1 - t1 * t1) + s * t2;
+    // Point in the disk space
+    Vector3 disk_N{t1, t2, sqrt(max(Real(0), 1 - t1*t1 - t2*t2))};
+
+    // Reprojection onto hemisphere -- we get our sampled normal in hemisphere space.
+    Frame hemi_frame(hemi_dir_in);
+    Vector3 hemi_N = to_world(hemi_frame, disk_N);
+
+    // Transforming the normal back to the ellipsoid configuration
+    return normalize(Vector3{alpha.x * hemi_N.x, alpha.y * hemi_N.y, max(Real(0), hemi_N.z)});
 }
